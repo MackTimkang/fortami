1c15154ad4fd4b516b1fc678f0261b9d580f1e81
MackTimkang <76024816+MackTimkang@users.noreply.github.com>
2022-12-15 11:53:07 +0800

    Css fix + add header

--- header.php
+++ header.php
@@ -5,17 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./Styles/header.css">
 </head>
 <body>
     <div class="mainhead">
         <div class="logo">
-            <img src="./src/FortamiLogo.png" alt="Fortami Logo" height="150px">
+            <img src="./src/FortamiLogo.png" alt="Fortami Logo">
         </div>
         <div class="option">
-            <a href="localhost/fortami/login.php">Login</a>
+            <a href="login.php">Login</a>
             <label for="seperator">|</label>
-            <a href="localhost/fortami/register.php">Register</a>
+            <a href="register.php">Register</a>
         </div>
     </div>
 </body>
--- index.php
+++ index.php
@@ -8,20 +8,34 @@ include 'header.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
     <title>Homepage</title>
+    <link rel="stylesheet" href="./Styles/style.css">
 </head>
 <body>
     <div class="maincont">
+        <div class="content1">
+            <h1>Cravings?</h1>
+            <form action="#" method="post">
+                <input type="submit" name="buynow" value="Buy now">
+            </form>
+        </div>
         <div class="buyer">
-            <img src="./src/Food-Delivery-Service-PNG-Photo.png" alt="buyer" width="300px">
+            <div class="buyerimg">
+                <img src="./src/Food-Delivery-Service-PNG-Photo.png" alt="buyer" >
+            </div>  
+        </div>
+        <div class="content2">
+            <h1>Food Shop Owner?</h1>
+            <form action="#" method="post">
+                <input type="submit" name="sellnow" value="Sell now">
+            </form>
         </div>
         <div class="seller">
-            <img src="./src/foodshop.png" alt="seller" width="300px">
+            <img src="./src/foodshop.png" alt="seller" >
         </div>
     </div>
+    <?php
+        include 'footer.php';
+    ?>
 </body>
 </html>
-<?php
-include 'footer.php';
-?>
